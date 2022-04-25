81c80e500bb3f930ae9ec4f5b5278ca817097a56
MNoori-coder <noorimohammad730@gmail.com>
2022-04-25 12:22:58 +0430

    test3

--- supermarket/models.py
+++ supermarket/models.py
@@ -2,7 +2,7 @@ from turtle import mode
 from django.db import models
 from django.contrib.auth.models import User
 
-#test2
+#test3
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)
