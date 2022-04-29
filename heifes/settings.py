9d0a6c8e5f8a82974ff5bd025317fe0173227c0b
negar <nnadian79@gmail.com>
2022-04-29 13:14:37 +0430

    fix some bugs

--- heifes/settings.py
+++ heifes/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'rest_framework',
+    'rest_framework.authtoken',
     'supermarket',
 ]
 
@@ -129,3 +131,4 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'supermarket.UserProfile'
--- supermarket/urls.py
+++ supermarket/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
-    path('product-list', views.product_list),
+    # path('product-list', views.product_list),
+    path('get-product', views.ListProduct.as_view()),
+    path('add-product', views.AddProduct.as_view())
 ]
