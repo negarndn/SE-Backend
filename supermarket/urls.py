6c0390487d9d704aece46e24d17518410ca8b7d9
MNoori-coder <noorimohammad730@gmail.com>
2022-05-06 15:21:49 +0430

    Third Commit

--- supermarket/urls.py
+++ supermarket/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
-    path('product-list', views.product_list),
+    # path('product-list', views.product_list),
+    path('get-category', views.ListCategory.as_view()),
+    path('get-product/<int:cat_id>/', views.ListProduct.as_view()),
+    path('add-product', views.AddProduct.as_view())
 ]
